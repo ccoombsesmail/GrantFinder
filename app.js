01a2a9355b7349f9b282ab92cc2170e08e56b42e
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-11-11 23:04:24 -0800

    Config React from prod

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express()
 const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser');
 const requiresAdmin = require('./middleware/admin')
+const path = require('path');
+
 //Importing Routes
 const users = require('./routes/api/users')
 const grants = require('./routes/api/grants')
+
 
 //Connect to Mongo
 mongoose
@@ -21,9 +24,15 @@ app.use(bodyParser.json())
 
 app.all('/api/grants/admin/*', requiresAdmin());
 // Routes
-app.get("/", (req, res) => res.send("Hello World"))
 app.use("/api/users", users)
 app.use("/api/grants", grants)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
