8109f6661ab3abcd6350ecae5e35f440a1dedd07
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-12-31 15:53:22 -0800

    Change static serve path in app.js

--- app.js
+++ app.js
@@ -29,17 +29,11 @@ app.use("/api/users", users)
 app.use("/api/grants", grants)
 app.use("/api/tags", tags)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-} else {
-  app.use(express.static('frontend/public'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
-  })
-}
+
+app.use(express.static('frontend/public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
